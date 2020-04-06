d3a04ee1faaef5c5cfafc112fc7536f3ed936e29
Ray Chung <ray.j.chung@gmail.com>
2020-04-05 22:14:05 -0700

    Add comments for checkForWin().

--- connect4.js
+++ connect4.js
@@ -151,15 +151,17 @@ function checkForWin() {
     );
   }
 
-  // TODO: read and understand this code. Add comments to help you.
-
+  // iterate through every position on the board
   for (var y = 0; y < HEIGHT; y++) {
     for (var x = 0; x < WIDTH; x++) {
+      // create possible 4 connected entries in different directions
       var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
       var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
       var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
       var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
+      // check if either if the direction has 4 connected entries
+      // by either player
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
       }
